6b8f12a619ed7fd40ac7646010787137bdd003c8
fdev31 <fdev31@gmail.com>
2021-02-11 23:39:40 +0100

    code cleanup

--- src/main.ino
+++ src/main.ino
@@ -1,12 +1,16 @@
-#ifdef USE_UNO
+// platform settings
+#ifdef USE_UNO // UNO NANO & compatibles
 #define RF_PIN 2
 #else
 #define RF_PIN G0 // G0 on M5 stack
 #include <M5StickC.h>
-#define USE_MENU
+#define USE_M5
 #define BUTTON_A G39
 #define BUTTON_B G37
+#define LONGPRESS_DURATION 300
 #endif
+
+// RF parameters
 #define PULSE_INTERVAL 24
 #define PREFIX 0x06ff // first byte is the remote ID (can be changed)
 #define PREFIXLEN 14
@@ -26,7 +30,7 @@ Control controls[] = {
     {"All", 0x2f9, 0xabd, 0},
 };
 
-const char* buttons[] = {"ON", "OFF", "TOGGLE"};
+const char* buttons[] = {"ON", "OFF", "Toggle"};
 #define NB_BUTTONS (sizeof(buttons)/sizeof(char*))
 int current_button = 2;
 
@@ -64,18 +68,15 @@ void sendKey(int code) {
 #endif
 }
 
-
 void setup() {
 #ifdef USE_UNO
     pinMode(LED_BUILTIN, OUTPUT);
 #else
-  M5.begin();
-  M5.Lcd.setRotation(1);
-  M5.Lcd.setTextSize(2);
-  M5.Lcd.setTextColor(WHITE,BLACK);
-  M5.Lcd.setCursor(40, 10, 2);
-  M5.Lcd.fillScreen(BLACK);
-  M5.Lcd.println("RCU");
+    M5.begin();
+    M5.Lcd.setRotation(1);
+    M5.Lcd.setTextSize(2);
+    M5.Lcd.setTextColor(ORANGE, BLACK);
+    M5.Lcd.fillScreen(ORANGE);
 #endif
     pinMode(RF_PIN, OUTPUT);
     digitalWrite(RF_PIN, LOW);
@@ -89,77 +90,47 @@ void sanity_check() {
 }
 
 void loop() {
-#ifdef USE_MENU
-  M5.update();
-  if (screen_dirty) {
-      M5.Lcd.setCursor(40, 10, 2);
-      M5.Lcd.fillScreen(BLACK);
-      M5.Lcd.println(controls[current_control].name);
-      M5.Lcd.println(buttons[current_button]);
-      screen_dirty = 0;
-  }
-
-  if (M5.BtnA.wasReleased()) {
-    if (ignore_A_release) {
-        ignore_A_release = 0;
-    } else {
-        current_control ++;
+#ifdef USE_M5
+    M5.update();
+    if (screen_dirty) {
+        M5.Lcd.setCursor(10, 10, 2);
+        M5.Lcd.fillScreen(BLACK);
+        M5.Lcd.println(controls[current_control].name);
+        M5.Lcd.setCursor(60, 20, 2);
+        M5.Lcd.println(buttons[current_button]);
+        screen_dirty = 0;
+    }
+    if (M5.BtnA.wasReleased()) {
+        if (ignore_A_release) {
+            ignore_A_release = 0;
+        } else {
+            current_control ++;
+            screen_dirty = 1;
+            if (current_control >= NB_CONTROLS) current_control = 0;
+            sanity_check();
+        }
+    }
+    if (M5.BtnB.wasPressed()) {
+        current_button ++;
         screen_dirty = 1;
-        if (current_control >= NB_CONTROLS) current_control = 0;
+        if (current_button >= NB_BUTTONS) current_button = 0;
         sanity_check();
     }
-  }
-  if (M5.BtnB.wasPressed()) {
-    current_button ++;
-    screen_dirty = 1;
-    if (current_button >= NB_BUTTONS) current_button = 0;
-    sanity_check();
-  }
-  if (M5.BtnA.pressedFor(500)) {
-    int code = 0;
-    switch (current_button) {
-          case 0:
-            code = controls[current_control].code_on;
-            break;
-          case 1:
-            code = controls[current_control].code_off;
-            break;
-          case 2:
-            code = controls[current_control].code_toggle;
-            break;
-      }
-      sendKey(code);
-      ignore_A_release = 1;
-  }
-
-#endif
-
-/*
-    sendKey(0xf7e); // 1 - toggle
-    sendKey(0x7fe); // 1 - on
-    sendKey(0xfb2); // 2 - off
-
-    sendKey(0x675); // 2 - toggle
-    sendKey(0xef5); // 2 - on
-    sendKey(0x6b9); // 2 - off
-
-    sendKey(0x574); // 3 - toggle
-    sendKey(0xdf4); // 3 - on
-    sendKey(0x5b8); // 3 - off
-
-    sendKey(0xc7b); // 4 - toggle
-    sendKey(0x4fb); // 4 - on
-    sendKey(0xcbf); // 4 - off
-
-    sendKey(0x2f9); // all on
-    sendKey(0xabd); // all off
-
-    if (pingpong++ %2 == 0) {
-        sendKey(0x2f9);
-        } else {
-        sendKey(0xabd);
+    if (M5.BtnA.pressedFor(LONGPRESS_DURATION)) {
+        int code = 0;
+        switch (current_button) {
+            case 0:
+                code = controls[current_control].code_on;
+                break;
+            case 1:
+                code = controls[current_control].code_off;
+                break;
+            case 2:
+                code = controls[current_control].code_toggle;
+                break;
         }
-    */
-
-
+        sendKey(code);
+        ignore_A_release = 1;
+    }
+#endif
 }
